35d6b690c03732cf792b2d830e8ff0bd3c1efd26
fakhriinaufal <fha.naufal06@gmail.com>
2021-01-09 00:16:16 +0700

    pemindahan script terpisah dari file html

---
+++ script.js
@@ -0,0 +1,17 @@
+let userMenu = document.querySelector("#navbar4")
+let username = sessionStorage.getItem("username")
+
+// membuat username menjadi title case
+username = username.replace(/\b\w/g, l => l.toUpperCase())
+
+userMenu.innerText = `${username}`
+
+function showMenuDropdown() {
+    let dropdown = document.querySelector(".dropItem")
+    dropdown.classList.toggle("show")
+}
+
+function userDropdown() {
+    let dropdown = document.querySelectorAll(".dropItem")[1]
+    dropdown.classList.toggle("show")
+}
